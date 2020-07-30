0a665d381ade87c283236ab41cc90e5fc5a0b9f9
Remicrazy <44520555+Remicrazy@users.noreply.github.com>
2020-07-30 20:10:29 +0200

    Setting Firebase

---
+++ jeunes_pousses/lib/screens/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyHomeScreen extends StatefulWidget {
+  @override
+  _MyHomeScreenState createState() => _MyHomeScreenState();
+}
+
+class _MyHomeScreenState extends State<MyHomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //
+        );
+  }
+}
